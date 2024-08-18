b4b60f6e382b6683ddcabdfc44e1d33145bcfea2
Okuyamaaa <soccer.south.komae.0705@gmail.com>
2024-08-18 16:33:01 +0900

    kadai5

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +14,13 @@ use App\Http\Controllers\PostController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get("/posts/create", [PostController::class, "create"]);
+
 
 Route::get('/posts', [PostController::class, "index"]);
+
+Route::post("/posts/store", [PostController::class, "store"])->name("posts.store");
+
 
 Route::get("/posts/{id}", [PostController::class, "show"]);
 ?>
